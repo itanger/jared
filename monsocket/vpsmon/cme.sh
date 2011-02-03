49923433fa1c836fbf29b9639c03da204fd31514
kunfirst@gmail.com <kunfirst@gmail.com@929dfb96-4fca-281e-b1e8-84114b6c9624>
2011-02-03 07:46:42 +0000

    add get_system_suage

--- monsocket/vpsmon/cme.sh
+++ monsocket/vpsmon/cme.sh
@@ -1,5 +1,3 @@
-#cme.sh----------------------------------------
-
 #!/bin/sh
 eth="eth0 eth1 "
 function get_eth_info()
@@ -18,14 +16,14 @@ function get_eth_info()
 }
 function sum_eth()
 {
- rm -fr 12321.txt
+ rm -fr 12321.txt.temp
  for name in $eth
  do
   ret=`get_eth_info $name`
   #echo $ret | awk '{bps+=$0;pps+$1;dps+=$2;print bps" "pps" "dps}'
-  echo $ret >> 12321.txt
+  echo $ret >> 12321.txt.temp
  done
- cat 12321.txt | awk 'BEGIN{bps=0;pps=0;dps=0}{bps+=$1;pps+=$2;dps+=$3}END{print bps" "pps" "dps}'
+ cat 12321.txt.temp | awk 'BEGIN{bps=0;pps=0;dps=0}{bps+=$1;pps+=$2;dps+=$3}END{print bps" "pps" "dps}'
 }
 flow=`sum_eth`
 disk=`df | awk 'BEGIN{total=0;avl=0;used=0;}NR > 1{total+=$2;used+=$3;avl+=$4;}END{printf"%d", avl/total*100}'`
@@ -34,22 +32,22 @@ mem=`top -b -n 1 | grep -w Mem | awk '{printf"%d",$6/$2*100}'`
  #top -b -n 1 | grep -w Cpu | awk '{print"cpu: "$5}' | awk -F '%' {print$1}
  cpu=`top -b -n 1 | grep -w Cpu | awk '{print$5}' | awk -F '%' '{printf"%d",$1}'`
  tm=`date +%s`
- if ! [ -f "flow.txt" ]
+ if ! [ -f "flow.txt.temp" ]
  then
- echo $tm" "$flow >flow.txt
+ echo $tm" "$flow >flow.txt.temp
  sleep 1
  tm=`date +%s`
  flow=`sum_eth`
  fi
- old_flow=`cat flow.txt`
+ old_flow=`cat flow.txt.temp`
  new_flow=`echo $tm $flow`
- echo $new_flow >flow.txt
+ echo $new_flow >flow.txt.temp
  #echo $old_flow
  #echo $new_flow
  final_flow=`echo $old_flow $new_flow | awk '{dif_tm=$5-$1;dif_byte=$6-$2;dif_pkt=$7-$3;dif_dpkt=$8-$4;printf"%d %d %d",dif_byte*8/dif_tm, dif_pkt/dif_tm, dif_dpkt/dif_tm}'`
  #final_flow=`echo $old_flow $new_flow | awk '{print$0}'`
- echo "cpu "$cpu
- echo "disk "$disk
- echo "mem "$mem
- echo "nic "$final_flow
+
+ rm -f *.temp
+
+ echo "cpu "$cpu" disk "$disk" mem "$mem" nic "$final_flow
 #------------------------en
---
+++ monsocket/vpsmon/get_system_usage.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+disk=`df | awk 'BEGIN{total=0;avl=0;used=0;}NR > 1{total+=$2;used+=$3;avl+=$4;}END{printf"%d", used/total*100}'`
+#top -b -n 1 | grep -w Mem | awk '{print"tot_mem: "$2" used_mem: " $4 " free_mem: "$6" left:"$6/$2"%"}'
+topout=`top -b -n 1`
+mem=`top -b -n 1 | grep -w Mem | awk '{printf"%d",$4/$2*100}'`
+#top -b -n 1 | grep -w Cpu | awk '{print"cpu: "$5}' | awk -F '%' {print$1}
+cpu=`top -b -n 1 | grep -w Cpu | awk '{print$5}' | awk -F '%' '{printf"%d",$1}'`
+declare -i cpu=100-$cpu
+
+echo "cpu "$cpu" disk "$disk" mem "$mem
