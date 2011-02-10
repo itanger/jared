8eac779ab55b621a38260dfb2741a0d585272ad4
kunfirst@gmail.com <kunfirst@gmail.com@929dfb96-4fca-281e-b1e8-84114b6c9624>
2011-02-10 10:40:20 +0000

    ready to publish

--- monsocket/vpsmon/ec2freecheck.sh
+++ monsocket/vpsmon/ec2freecheck.sh
@@ -3,8 +3,7 @@ thismonth=`date +"%b '%y"`
 usage=`vnstat -m | grep "$thismonth" | awk '{print $9}'`
 usage=`echo $usage | awk '{printf "%d",$usage}' `
 # max traffic monthly is 20G
-#max=$((20*1024)) 
-max=$((20*1)) 
+max=$((20*1024)) 
 echo $usage" of "$max
 if [ $usage -gt $max ]; then
 	echo "Warning: exceed max traffic"
